ca5a446d03a9e2061cf6f704d627b12e7693fc81
KAMILKAPLAN\KAMILKAPLAN <kamilkaplnn@gmail.com>
2022-10-08 14:20:39 +0300

    3_React

---
+++ 3_React/public/scripts/app.js
@@ -0,0 +1,9 @@
+"use strict";
+
+var root = document.getElementById("root");
+/* React */
+
+var template = React.createElement('h1', null, 'My first react app!');
+/* React DOM */
+
+ReactDOM.render(template, root);
---
+++ 3_React/src/app.js
@@ -0,0 +1,11 @@
+var root = document.getElementById("root");
+
+/* React */
+var template = React.createElement(
+    'h1',
+    null,
+    'My first react app!'
+);
+
+/* React DOM */
+ReactDOM.render(template, root);
