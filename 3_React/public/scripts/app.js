0a13e8182362e7d45fe7a96a7c015dfe5e5770e4
KAMILKAPLAN\KAMILKAPLAN <kamilkaplnn@gmail.com>
2022-10-08 15:08:41 +0300

    jsx expression

--- 3_React/public/scripts/app.js
+++ 3_React/public/scripts/app.js
@@ -3,9 +3,33 @@
 var root = document.getElementById("root");
 /* JSX - Javascript XML = JS içindeki etiketleri birer obje gibi yazmamızı sağlıyor */
 
-var template = /*#__PURE__*/React.createElement("h1", null, "My first react app!");
-/* React */
+var productName = "Samsung S10";
+var productPrice = 4000;
+var productDescription = "bu bir telefon";
 
+function formatPrice(p) {
+  return p + " TL";
+}
+
+var product = {
+  name: "Samsung S10",
+  price: 4000,
+  description: "bu bir telefon"
+};
+
+function formatPrice2(p) {
+  return p.price + " TL";
+}
+
+var template = /*#__PURE__*/React.createElement("div", {
+  id: "productDetail"
+}, /*#__PURE__*/React.createElement("h1", {
+  id: "productName"
+}, "Name : ", productName), /*#__PURE__*/React.createElement("p", {
+  id: "productPrice"
+}, "Price : ", formatPrice2(product)), /*#__PURE__*/React.createElement("p", {
+  id: "productDescription"
+}, "Description : ", productDescription));
 /* React DOM */
 
 ReactDOM.render(template, root);
--- 3_React/src/app.js
+++ 3_React/src/app.js
@@ -1,8 +1,30 @@
 var root = document.getElementById("root");
 
 /* JSX - Javascript XML = JS içindeki etiketleri birer obje gibi yazmamızı sağlıyor */
-var template = <h1>My first react app!</h1>;
-/* React */
+var productName = "Samsung S10";
+var productPrice = 4000;
+var productDescription = "bu bir telefon";
+
+function formatPrice(p) {
+    return p + " TL";
+}
+
+var product = {
+    name: "Samsung S10",
+    price: 4000,
+    description: "bu bir telefon"
+}
+
+function formatPrice2(p) {
+    return p.price + " TL";
+}
+
+var template =
+    <div id="productDetail">
+        <h1 id="productName">Name : {productName}</h1>
+        <p id="productPrice">Price : {formatPrice2(product)}</p>
+        <p id="productDescription">Description : {productDescription}</p>
+    </div>;
 
 /* React DOM */
 ReactDOM.render(template, root);
